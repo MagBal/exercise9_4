c4a7d98823d2a9767a723bd384e60f7d36708931
Magda Balun <magi@op.pl>
2017-12-05 19:54:41 +0100

    Change .js file

--- scripts_v2.js
+++ scripts_v2.js
@@ -1,17 +1,17 @@
-var n = 10;
-function drawTree(n) {
-	for (var i=0; i<n; i++) {
+var treeHeight = 10;
+function drawTree(treeHeight) {
+	for (var i = 0; i < treeHeight; i++) {
   		var star = '';
-  		for (var j=i; j<n; j++) {
+  		for (var j = i; j < treeHeight; j++) {
      		star += ' ';
       	}
-      	for (k=0; k<(2*i+1); k++) {
+      	for (var k = 0; k < (2 * i + 1); k++) {
       		star += '*'
       	}
       	console.log(star);
    	}
 }
-drawTree(n);
+drawTree(treeHeight);
 
 
 
